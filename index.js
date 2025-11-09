4d24e191b1e15587763ae575c8fbb734c4bb290e
mehedi hasan <mhshohan04@gmail.com>
2025-11-09 22:47:25 +0600

    create basic server and connected to mongodb database

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json())
+
+// 2L0y8sfPoBYbis7U
+const uri = "mongodb+srv://pawmartDb:2L0y8sfPoBYbis7U@cluster0.5yczeea.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Server is running!')
+})
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('listingdb')
+    const listingCollection = db.collection('listings')
+
+    // find all listing data from mongodb
+     app.get('/listings', async (req, res) => {
+      try {
+        const cursor = listingCollection.find();
+        const listings = await cursor.toArray();
+        res.json(listings);
+      } catch (error) {
+        console.error(' Error fetching listings:', error);
+        res.status(500).json({ message: 'Error fetching listings' });
+      }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.db("pawmart").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
